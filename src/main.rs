07e24cd97d8e231c8f85377e9942ac6d95882e57
Kenechukwu Nnamani <kenechukwunnamani@Kenechukwus-MacBook-Pro.local>
2021-03-14 18:39:08 +0100

    set cli commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::{thread, time};
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "pipe", about = "Read logs from multiple sources and output to elasticsearch")]
+struct Pipe {
+    #[structopt(subcommand)]
+    cmd: Command
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    #[structopt(name = "check", about = "check the validity of the config file")]
+    Check(Config),
+
+    #[structopt(name = "stream", about = "Activate log streaming to elasticsearch")]
+    Stream(Config),
+}
+
+#[derive(Debug, StructOpt)]
+struct Config {
+    #[structopt(short = "c", long = "config", parse(from_os_str))]
+    config: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let timer = time::Duration::from_secs(1);
+    let config = Pipe::from_args();
+    loop {
+        thread::sleep(timer);
+
+        println!("Reading the target!");
+        println!("{:?}", config);
+    }
 }
