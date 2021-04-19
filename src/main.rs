d6b361ff4fbb5b908dbf38b5603aa2322fb6da19
Kenec <nnamani.kenechukwu@gmail.com>
2021-04-19 20:53:22 +0200

    deserialize config

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use crate::collector::streamer;
+use crate::collector::streamer::Streamer;
 use std::path::PathBuf;
 use structopt::StructOpt;
+use crate::config::Config;
 
 mod collector;
+mod config;
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -17,28 +19,33 @@ struct Pipe {
 #[derive(Debug, StructOpt)]
 enum Command {
     #[structopt(name = "check", about = "check the validity of the config file")]
-    Check(Config),
+    Check(ConfigPath),
 
     #[structopt(name = "stream", about = "Activate log streaming to elasticsearch")]
-    Stream(Config),
+    Stream(ConfigPath),
 }
 
 #[derive(Debug, StructOpt)]
-struct Config {
+pub struct ConfigPath {
     #[structopt(short = "c", long = "config", parse(from_os_str))]
     config: PathBuf,
 }
 
 #[tokio::main]
 async fn main() {
-    let config = Pipe::from_args();
+    env_logger::init();
+    let pipe_config = Pipe::from_args();
 
-    match &config.cmd {
-        Command::Check(config) => println!(
+    match pipe_config.cmd {
+        Command::Check(configPath) => {
+            let config = Config::load(&configPath.config).unwrap();
+            println!("{:?}", config.destination.elasticsearch.host);
+            println!("{:?}", config.sources.files.logs);
+            println!(
             "You are checking if the config file {:?} and the elasticsearch connections are okay!",
-            config
-        ),
+            configPath
+        )},
 
-        Command::Stream(_config) => streamer::stream().await,
+        Command::Stream(configPath) => Streamer::new(configPath).stream().await
     }
 }
